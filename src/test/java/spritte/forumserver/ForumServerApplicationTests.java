002874f6b4323d66f8a0d5496025bc1c68468af7
spritte <a8009608ab@gmail.com>
2024-07-29 17:24:26 +0900

    init

---
+++ src/test/java/spritte/forumserver/ForumServerApplicationTests.java
@@ -0,0 +1,13 @@
+package spritte.forumserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ForumServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
